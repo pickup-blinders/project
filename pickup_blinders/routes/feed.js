b8e7959f6ea1b2a8346f67154477a6aab62d009f
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-07-06 18:30:37 +0200

    Voting Up and Down with Bug

--- pickup_blinders/routes/feed.js
+++ pickup_blinders/routes/feed.js
@@ -3,23 +3,29 @@ const Post = require('../models/Post');
 const User = require('../models/User');
 const { populate } = require('../models/User');
 
-const router  = express.Router();
+const router = express.Router();
 
 router.get('/feed/funny', (req, res, next) => {
-  Post.find({category: "funny"}).sort({created_at: -1}).populate("userid").then(post => {
-    res.render('funny', {post: post});
+  Post.find({ category: "funny" }).sort({ created_at: -1 }).populate("userid").then(post => {
+    res.render('funny', { post: post });
   }).catch(err => {
     console.log(err)
   })
 });
 
 router.get('/feed/tinder', (req, res, next) => {
-  Post.find({category: "tinder"}).populate("userid").then(post => {
-    res.render('funny', {post: post});
+  Post.find({ category: "tinder" }).populate("userid").then(post => {
+    res.render('funny', { post: post });
   }).catch(err => {
     console.log(err)
   })
 });
+
+router.post("/vote/:id", (req, res) => {
+  console.log(req.body)
+  console.log("this is recieved from the front end", req.params.id)
+  Post.findByIdAndUpdate(req.params.id)
+})
 
 
 
