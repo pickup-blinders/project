fc3ae7ebf11430c3185996811a8a65c78ea579d1
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-07-08 17:07:09 +0200

    add new connection for DB in app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash = require("connect-flash");
 
 
 mongoose
-  .connect('mongodb://localhost/pickup-blinders', { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/pickup-blinders', { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
